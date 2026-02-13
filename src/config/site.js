c23b59f885d85ad71e2191fc72a34b2b1d7a2e63
John Furr <admin@Mac.attlocal.net>
2026-02-13 03:10:22 -0600

    linked resume and changed about page

--- src/config/site.js
+++ src/config/site.js
@@ -1,33 +1,30 @@
 // Get site URL from environment variable, use default value if not set
 // Note: Please set the correct PUBLIC_SITE_URL in .env file after first deployment
-const SITE_URL = import.meta.env.PUBLIC_SITE_URL || 'https://portfolio.ricoui.com/';
+const SITE_URL = import.meta.env.PUBLIC_SITE_URL || 'https://john-furr.com/';
 
 export const siteConfig = {
-	title: "Ricoui Portfolio",
-	author: "Ricoui",
+	title: "John Furr",
+	author: "John",
 	url: SITE_URL,
-	mail: "hello@ricoui.com",
+	mail: "furrm.john@gmail.com",
 	// resume add your resume file path here: /assets/resume.pdf
-	resume: "https://ricoui.com/",
+	resume: "/assets/resume.pdf",
 	utm: {
 		source: `${SITE_URL}`,
 		medium: "referral",
 		campaign: "navigation",
 	},
 	meta:{
-		title: "Ricoui Portfolio",
-		description: "I'm Rico, a web designer passionate about both design and code. Currently developing a personal product for the design community.",
-		keywords: "web designer, portfolio, design, code, personal website",
+		title: "John Furr",
+		description: "I'm John, a web developer and designer",
+		keywords: "web developer, web designer, portfolio, design, code, personal website",
 		image: `${SITE_URL}/og.jpg`,
 		twitterHandle: "ricouii",
 	},
 	// social links
 	social:{
-		twitter: "https://x.com/ricouii",
-		twitterName: "ricouii",
+		linkedin: "https://www.linkedin.com/in/johnfurr/",
 		github: "https://github.com/ricocc",
-		blog: "https://ricoui.com",
-		xiaohongshu:"https://www.xiaohongshu.com/user/profile/5f2b6903000000000101f51f"
 	},
 };
 
